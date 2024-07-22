d7d1f4ddbb2ea37e712f23c00c673c103f24100a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-07-22 09:25:43 +0200

    update

--- chapter8/index.js
+++ chapter8/index.js
@@ -1,1 +1,61 @@
 "use strict";
+
+/****************GLOBAL METHODS********** */
+//ENCODING AND DECODING URIs
+let uri = "https://www.example.com/submit?name=maaike van putten";
+// let encoded_uri = encodeURI(uri);
+// console.log("Encoded:", encoded_uri);
+// let decoded_uri = decodeURI(encoded_uri);
+// console.log("Decoded:", decoded_uri);
+
+//ENCODING AND DECODING URI COMPONENTS
+// let encoded_uri = encodeURIComponent(uri);
+// console.log("Encoded:", encoded_uri);
+// let decoded_uri = decodeURIComponent(encoded_uri);
+// console.log("Decoded:", decoded_uri);
+
+//PARSING NUMBERS (PARSEINT())
+// let str_int = "6";
+// let int_int = parseInt(str_int);
+// console.log("Type of ", int_int, " is ", typeof int_int);
+
+// let str_nan = "hello!";
+// let int_nan = parseInt(str_nan);
+// console.log("Type of", int_nan, "is", typeof int_nan);
+
+// let str_float = "7.6";
+// let float_float = parseFloat(str_float);
+// console.log("Type of", float_float, "is", typeof float_float);
+
+// let str_int = "6";
+// let float_int = parseFloat(str_int);
+// console.log("Type of", float_int, "is", typeof float_int);
+
+// let str_binary = "0b101";
+// let float_binary = parseFloat(str_binary);
+// console.log("Type of", float_binary, "is", typeof float_binary);
+
+// let str_nan = "hello!";
+// let float_nan = parseFloat(str_nan);
+// console.log("Type of", float_nan, "is", typeof float_nan);
+
+//EVAL()
+
+
+
+
+
+
+/****************ENCODING AND DECODING URIs********** */
+
+/****************ENCODING AND DECODING URIs********** */
+
+/****************ENCODING AND DECODING URIs********** */
+
+/****************ENCODING AND DECODING URIs********** */
+
+/****************ENCODING AND DECODING URIs********** */
+
+/****************ENCODING AND DECODING URIs********** */
+
+/****************ENCODING AND DECODING URIs********** */
