31864e3cf6d5ac8aeac9821344b3b13ad98f7d60
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-07-22 18:37:25 +0200

    update

--- chapter9/index.js
+++ chapter9/index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+/***********HISTORY OBJECT********* */
+// console.dir(window);//entire window object
+// console.dir(window.history.length);//number (history length)
+// console.dir(history);
+
+/***********NAVIGATOR OBJECT********* */
+//console.dir(window.navigator);
+
+/***********LOCATION OBJECT********* */
+// console.dir(location);
+
+/***********THE DOM********* */
+console.dir(document);
+
+const e1 = document.querySelector("h1");
+console.dir(e1);
+
+const allE = document.querySelectorAll(".output");
+console.log(allE);
