de6eda0e678bbac7907168785703b254f4b38b80
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-07-26 10:28:41 +0200

    update

--- randomNameAlert/index.js
+++ randomNameAlert/index.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let students = [
+    "Juan",
+    "Alex",
+    "Michael",
+    "Bhabha",
+    "Asanda",
+    "Thabo",
+    "Sbahle",
+    "Lucky",
+    "Diego",
+    "Boitumelo",
+    "Sharon",
+    "Vhukhudo",
+    "Happy",
+    "Sandile",
+    "Fortune"
+];
