e3a24391cf034b53274a7fc237eb6d4b01b11491
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-08-13 16:55:33 +0200

    first commit

---
+++ chapter1/chapter1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert("Saying hi from a different file!");
+
+/*
+let status = "intermediate";
+let scared = false;
+//triple equals to signs
+//same value AND same data type
+if (status === "new") {
+    console.log("Welcome to JavaScript!");
+    if (scared) {
+        console.log("Don't worry you will be fine!");
+    } else {
+        console.log("You're brave! You are going to do great!");
+    }
+} else {
+    console.log("Welcome back, I knew you'd like it!");
+}
+*/
+
+// let myName = prompt("What is your name?");
+// console.log("Hi there, " + myName);
+
+// console.log(Math.random());//0 - 1
+//console.log(Math.floor(Math.random() * 100));//0 - 100
+console.log(Math.floor((Math.random() * 100 - 1) + 1));//1 - 100
